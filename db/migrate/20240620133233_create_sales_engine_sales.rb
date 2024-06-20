06b37916b6ce2ac8cbadb0911f2444b49c80d1bf
amira2011 <shaikh.abidali@gmail.com>
2024-06-20 19:53:32 +0530

    Updated Sales Table Name

--- db/migrate/20240620133233_create_sales_engine_sales.rb
+++ db/migrate/20240620133233_create_sales_engine_sales.rb
@@ -1,6 +1,6 @@
-class CreateSalesEngineSales < ActiveRecord::Migration[7.1]
+class CreateSales < ActiveRecord::Migration[7.1]
   def change
-    create_table :sales_engine_sales do |t|
+    create_table :sales do |t|
       t.string :product_name
       t.string :amount
 
--- lib/sales_engine/version.rb
+++ lib/sales_engine/version.rb
@@ -1,3 +1,3 @@
 module SalesEngine
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
